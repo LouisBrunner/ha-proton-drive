02e15e3f1a764b93c7ab35d48059fb50dd001f72
Louis Brunner <louis.brunner.fr@gmail.com>
2025-04-25 11:52:13 +0100

    fix: more issues during auth refresh

--- custom_components/proton_drive/api.py
+++ custom_components/proton_drive/api.py
@@ -3,7 +3,11 @@
 from __future__ import annotations
 
 import json
-from typing import TYPE_CHECKING, Any
+from contextlib import (
+    AsyncContextDecorator,
+    ContextDecorator,
+)
+from typing import TYPE_CHECKING, Any, Self
 
 import aiofiles
 import aiofiles.os
@@ -16,6 +20,7 @@ from .const import LOGGER
 
 if TYPE_CHECKING:
     from collections.abc import AsyncIterator, Callable, Coroutine
+    from types import TracebackType
 
     from homeassistant.core import HomeAssistant
 
@@ -42,6 +47,53 @@ class ProtonDriveAPIMFAError(
     """Exception to indicate a multi-factor authentication error."""
 
 
+class _ErrorCatcher(ContextDecorator, AsyncContextDecorator):
+    def __enter__(self) -> Self:
+        """Enter the context manager."""
+        return self
+
+    def __exit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> bool:
+        """Exit the context manager."""
+        return self.__handle_exceptions(exc_type, exc_val, exc_tb)
+
+    async def __aenter__(self) -> Self:
+        """Enter the context manager."""
+        return self
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> bool:
+        """Exit the context manager."""
+        return self.__handle_exceptions(exc_type, exc_val, exc_tb)
+
+    def __handle_exceptions(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        _exc_tb: TracebackType | None,
+    ) -> bool:
+        if exc_type is None or not issubclass(exc_type, RuntimeError):
+            return False
+        LOGGER.exception("proton API call failed")
+        e = exc_val
+        if "Code=9001" in str(e):
+            msg = f"MFA Error: {e}"
+            raise ProtonDriveAPIMFAError(msg) from e
+        if "Code=8002" in str(e) or "Code=10013" in str(e):
+            msg = f"Authentication Error: {e}"
+            raise ProtonDriveAPIAuthenticationError(msg) from e
+        msg = f"API Error: {e}"
+        raise ProtonDriveAPIError(msg) from e
+
+
 class ProtonDriveClient:
     """Client for the Proton Drive API."""
 
@@ -58,23 +110,24 @@ class ProtonDriveClient:
         """Client for the Proton Drive API."""
         self._hass = hass
         self._instance_id = instance_id
-        self._client = NewClient(
-            creds,
-            update_creds,
-        )
-        if share_id != "":
-            self._client.SelectShare(share_id)
+        with _ErrorCatcher():
+            self._client = NewClient(
+                creds,
+                update_creds,
+            )
+            if share_id != "":
+                self._client.SelectShare(share_id)
         self._folder = base_folder
 
     async def list_shares(self) -> list[Share]:
         """List available share drives."""
-        return await self.__handle_errors(
+        return await self.__call_api(
             hass=self._hass, call=lambda: self._client.ListShares()
         )
 
     async def __create_root_folder_if_needed(self) -> Folder:
         """Create the Home Assistant folder if it doesn't exist."""
-        return await self.__handle_errors(
+        return await self.__call_api(
             hass=self._hass, call=lambda: self._client.MakeRootFolder(self._folder)
         )
 
@@ -82,7 +135,7 @@ class ProtonDriveClient:
         """Get a Proton Drive link ID based on the Home Assistant backup ID."""
         folder = await self.__create_root_folder_if_needed()
         try:
-            return await self.__handle_errors(
+            return await self.__call_api(
                 hass=self._hass,
                 call=lambda: folder.FindBackup(self._instance_id, backup_id),
             )
@@ -91,7 +144,7 @@ class ProtonDriveClient:
 
     async def download_backup(self, link_id: str) -> AsyncIterator[bytes]:
         """Download a Home Assistant backup using the Proton Drive link ID."""
-        file_path = await self.__handle_errors(
+        file_path = await self.__call_api(
             hass=self._hass,
             call=lambda: self._client.DownloadFile(link_id),
         )
@@ -114,7 +167,7 @@ class ProtonDriveClient:
                 await f.write(chunk)
             await f.flush()
 
-            await self.__handle_errors(
+            await self.__call_api(
                 hass=self._hass,
                 call=lambda: folder.Upload(
                     self._instance_id,
@@ -127,7 +180,7 @@ class ProtonDriveClient:
 
     async def delete_backup(self, link_id: str) -> None:
         """Delete a Home Assistant backup using the Proton Drive link ID."""
-        await self.__handle_errors(
+        await self.__call_api(
             hass=self._hass,
             call=lambda: self._client.DeleteFile(link_id),
         )
@@ -135,28 +188,16 @@ class ProtonDriveClient:
     async def list_backups(self) -> list[AgentBackup]:
         """List Home Assistant backups."""
         folder = await self.__create_root_folder_if_needed()
-        metadatas = await self.__handle_errors(
+        metadatas = await self.__call_api(
             hass=self._hass,
             call=lambda: folder.ListFilesMetadata(self._instance_id),
         )
         return [AgentBackup.from_dict(json.loads(metadata)) for metadata in metadatas]
 
     @classmethod
-    async def __handle_errors(
-        cls, *, hass: HomeAssistant, call: Callable[[], Any]
-    ) -> Any:
-        try:
+    async def __call_api(cls, *, hass: HomeAssistant, call: Callable[[], Any]) -> Any:
+        async with _ErrorCatcher():
             return await hass.async_add_executor_job(call)
-        except RuntimeError as e:
-            LOGGER.exception("proton API call failed")
-            if "Code=9001" in str(e):
-                msg = f"MFA Error: {e}"
-                raise ProtonDriveAPIMFAError(msg) from e
-            if "Code=8002" in str(e):
-                msg = f"Authentication Error: {e}"
-                raise ProtonDriveAPIAuthenticationError(msg) from e
-            msg = f"API Error: {e}"
-            raise ProtonDriveAPIError(msg) from e
 
     @classmethod
     async def login(
@@ -168,7 +209,7 @@ class ProtonDriveClient:
         mfa: str | None = None,
     ) -> Credentials:
         """Get credentials from."""
-        return await cls.__handle_errors(
+        return await cls.__call_api(
             hass=hass,
             call=lambda: Login(username, password, mfa if mfa is not None else ""),
         )
